0ab7c6d0f34cae25b58ebfb9407d4b975cdcb421
lucasxas <lucasxas@gmail.com>
2015-03-10 11:45:10 +1300

    ops... errei o nome do método! hahaha

--- LimiteDaZoeira/src/Main.java
+++ LimiteDaZoeira/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String args[]){
 		Zoeira z = new Zoeira();
 		
-		z.getZoeira();
+		z.getLimite();
 	}
 
 }
--- LimiteDaZoeira/src/Zoeira.java
+++ LimiteDaZoeira/src/Zoeira.java
@@ -1,7 +1,7 @@
 
 public class Zoeira {
 	
-	public void getZoeira(){
+	public void getLimite(){
 		System.out.println("Operação ilegal:");
 		System.out.flush();
 		System.err.println("Não é possível obter o limite da Zoeira, porque a Zoeira não tem limite.");
